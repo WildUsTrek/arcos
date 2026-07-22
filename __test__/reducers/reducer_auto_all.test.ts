3858ef091500485d56268ad7f2eee4a64f864c0b
Wild_U <baffiwild@gmail.com>
2026-07-22 23:00:54 +0200

    Fix reducer contract test action type

--- __test__/reducers/reducer_auto_all.test.ts
+++ __test__/reducers/reducer_auto_all.test.ts
@@ -1,8 +1,9 @@
 import { expect, test } from 'bun:test'
+import { INIT } from '../../src/constants/ActionTypes'
 import reducers from '../../src/reducers'
 
 test('initial store matches the offline campaign contract', () => {
-  const state = reducers(undefined, {})
+  const state = reducers(undefined, { type: INIT })
 
   expect(state.lang.code).toBe('it')
   expect(state.campaign).toEqual({
